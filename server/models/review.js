acdec3ea5f9c55a6c978f19ea9af1e9303f262a2
calebpenning <calebpenning@mail.com>
2021-09-14 17:44:49 -0700

    cleaned up some queries

--- server/models/review.js
+++ server/models/review.js
@@ -5,31 +5,34 @@ const User = require('./user')
 
 class Review {
     static async create({movieID, userID, rating, title, body}) {
-        let movieCheck = await db.query(
-            `SELECT id, title FROM movies WHERE id = $1`,
-            [movieID]
-        )
-        if (!movieCheck.rows[0]) {
-            let movie = await Movie.getMovieByID(movieID)
-            let title = movie.result.title
-            await Movie.create({id: movieID, title})
+        try {
+            let movieCheck = await db.query(
+                `SELECT id, title FROM movies WHERE id = $1`,
+                [movieID]
+            )
+            if (!movieCheck.rows[0]) {
+                let movie = await Movie.getMovieByID(movieID)
+                let title = movie.result.title
+                await Movie.create({id: movieID, title})
+            }
+            console.log(movieCheck)
+    
+            let res = await db.query(
+                `INSERT INTO reviews
+                (movie_id, user_id, rating, title, body)
+                VALUES ($1, $2, $3, $4, $5)
+                RETURNING id, movie_id AS "movieID", user_id AS "userID", rating, title, body, created_at AS "createdAt"`,
+                [movieID, userID, rating, title, body]
+            )
+            console.log(res)
+    
+            if (res.rows[0]) return ({ created: res.rows[0]})
+            else return new ExpressError(`unable to review movie with ID ${movieID}`, 400)  
         }
-        console.log(movieCheck)
-
-        let res = await db.query(
-            `INSERT INTO reviews
-            (movie_id, user_id, rating, title, body)
-            VALUES ($1, $2, $3, $4, $5)
-            RETURNING id, movie_id AS "movieID", user_id AS "userID", rating, title, body, created_at AS "createdAt"`,
-            [movieID, userID, rating, title, body]
-        )
-        console.log(res)
-
-        if (res.rows[0]) return ({ created: res.rows[0]})
-        else return ({
-            success: false,
-            message: `unable to review movie with ID ${movieID}`
-        })
+        catch(e) {
+            console.error(e)
+            return e
+        }
 
     }
 
@@ -42,20 +45,22 @@ class Review {
                 reviews.user_id AS "userID", 
                 reviews.rating, 
                 reviews.title, reviews.body, 
-                reviews.created_at, 
+                reviews.created_at AS "createdAt", 
                 movies.id AS "movieID", 
                 movies.title,
                 users.username
                 FROM reviews, movies, users
                 WHERE reviews.movie_id = movies.id 
                 AND reviews.user_id = $1
-                AND users.id = $1`,
+                AND users.id = $1
+                ORDER BY reviews.created_at ASC`,
                 [userID]
             )
             return res.rows
         }
         catch(e) {
             console.error(e)
+            return e
         }
     }
     // Get full review info, necessary user info, and necessary movie info for components
@@ -66,19 +71,23 @@ class Review {
                 `SELECT 
                 reviews.id as "reviewID", 
                 reviews.rating, reviews.title, 
-                reviews.body, reviews.created_at, 
+                reviews.body, 
+                reviews.created_at AS "createdAt", 
                 users.id AS "userID", 
                 users.username, movies.title, 
                 movies.id AS "movieID"
                 FROM reviews, users, movies
-                WHERE reviews.movie_id = $1 AND reviews.user_id = users.id AND movies.id = reviews.movie_id
+                WHERE reviews.movie_id = $1 
+                AND reviews.user_id = users.id 
+                AND movies.id = reviews.movie_id
                 ORDER BY reviews.created_at ASC`, [movieID]
             )
             return res.rows
         }
 
         catch(e) {
-            console.log(e)
+            console.error(e)
+            return e
         }
     }
 }
